b8e0973b7bb99644c9a6c8a12d77564ed458fdad
Ichsan Ramadhan <ichsan.ramadhan32@gmail.com>
2024-03-03 11:38:30 +0700

    feat: make cariTeman function

--- main.go
+++ main.go
@@ -7,6 +7,20 @@ type Teman struct {
 	Alasan    string
 }
 
+func cariTeman(absen int) Teman {
+	daftarTeman := map[int]Teman{
+		1: {"Roni", "Jl. Merdeka No. 1", "Mahasiswa", "Ingin belajar bahasa pemrograman Go"},
+		2: {"Bujang", "Jl. Damai No. 10", "Designer", "Ingin memperluas skill programming"},
+		3: {"Anton", "Jl. Sejahtera No. 5", "Developer", "Membutuhkan bahasa pemrograman baru untuk pekerjaan"},
+	}
+
+	teman, found := daftarTeman[absen]
+	if !found {
+		return Teman{}
+	}
+	return teman
+}
+
 func main() {
 
 }
